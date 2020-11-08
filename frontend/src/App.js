be74ae3c9e33597ef394bed504f6918a6de781c7
gadaharon <gadaharon91@gmail.com>
2020-11-08 20:32:18 +0200

    add navbar, add footer, set main tag min-height

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
